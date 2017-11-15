feb17fa6bdc79f09a10878b85beb62cb19542d78
RBI4 <rebecca.inge@gmail.com>
2017-11-15 19:55:19 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,24 @@
 'use strict';
+
+class Question{
+  constructor(){
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.find(this)
+  }
+
+  static All(){
+    return this._All;
+  }
+}
+
+static FindByID(Id){
+  return.this.All().find(function(question){
+    return question.id === Id;
+    })
+  }
+}
+
+User._All = []
